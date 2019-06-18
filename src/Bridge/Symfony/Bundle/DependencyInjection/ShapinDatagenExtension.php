e369aaaa2d3dbf95cedc525ef5c3026802605779
Olivier Dolbeau <git@a.bbnt.me>
2019-06-18 16:36:43 +0200

    Split processor config to load them only when needed

--- src/Bridge/Symfony/Bundle/DependencyInjection/ShapinDatagenExtension.php
+++ src/Bridge/Symfony/Bundle/DependencyInjection/ShapinDatagenExtension.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Shapin\Datagen\Bridge\Symfony\Bundle\DependencyInjection;
 
+use Doctrine\DBAL\Connection;
+use Shapin\Stripe\StripeClient;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
@@ -18,5 +20,12 @@ final class ShapinDatagenExtension extends Extension
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+
+        if (class_exists(Connection::class)) {
+            $loader->load('processor_dbal.xml');
+        }
+        if (class_exists(StripeClient::class)) {
+            $loader->load('processor_stripe.xml');
+        }
     }
 }
