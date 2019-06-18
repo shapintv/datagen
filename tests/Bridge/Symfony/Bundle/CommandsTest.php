ff24deba8ec132ccf44cb9b5917e73bd8fadee9c
Olivier Dolbeau <git@a.bbnt.me>
2019-06-18 14:33:06 +0200

    Use transactions for commands est

--- tests/Bridge/Symfony/Bundle/CommandsTest.php
+++ tests/Bridge/Symfony/Bundle/CommandsTest.php
@@ -4,33 +4,31 @@ declare(strict_types=1);
 
 namespace Shapin\Datagen\Tests\Bridge\Symfony\Bundle;
 
+use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\ApplicationTester;
 
 class CommandsTest extends KernelTestCase
 {
-    public function setUp(): void
-    {
-        $file = __DIR__.'/../../../app/var/cache/test/db.sqlite';
-
-        if (file_exists($file)) {
-            unlink($file);
-        }
-    }
-
     public function testExecute()
     {
         $tester = $this->getTester();
 
+        $this->getConnection()->beginTransaction();
+
         $tester->run(['command' => 'shapin:datagen:load']);
         $this->assertSame(0, $tester->getStatusCode());
         $this->assertContains('[OK] Job DONE!', $tester->getDisplay());
+
+        $this->getConnection()->rollback();
     }
 
     public function testExecuteSchemaOnlythenFixturesOnly()
     {
         $tester = $this->getTester();
+
+        $this->getConnection()->beginTransaction();
 
         $tester->run(['command' => 'shapin:datagen:load', '--dbal-schema-only' => null]);
         $this->assertSame(0, $tester->getStatusCode());
@@ -39,6 +37,8 @@ class CommandsTest extends KernelTestCase
         $tester->run(['command' => 'shapin:datagen:load', '--dbal-fixtures-only' => null]);
         $this->assertSame(0, $tester->getStatusCode());
         $this->assertContains('[OK] Job DONE!', $tester->getDisplay());
+
+        $this->getConnection()->rollback();
     }
 
     private function getTester(): ApplicationTester
@@ -50,5 +50,13 @@ class CommandsTest extends KernelTestCase
         $application->setAutoExit(false);
 
         return new ApplicationTester($application);
+    }
+
+    private function getConnection(): Connection
+    {
+        // static::$container is not defined with SF < 4
+        $container = isset(static::$container) ? static::$container : static::$kernel->getContainer();
+
+        return $container->get('doctrine.dbal.default_connection');
     }
 }
