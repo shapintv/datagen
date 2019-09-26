ce87bb504f5c8156826acb23b1b4321787cb37db
Olivier Dolbeau <git@a.bbnt.me>
2019-09-26 16:17:00 +0200

    Remove unused dependencies

--- tests/app/Kernel.php
+++ tests/app/Kernel.php
@@ -12,7 +12,6 @@ class Kernel extends BaseKernel
         return array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Http\HttplugBundle\HttplugBundle(),
             new Shapin\Datagen\Bridge\Symfony\Bundle\ShapinDatagenBundle(),
             new Shapin\Datagen\Tests\Fixtures\TestBundle\TestBundle(),
         );
